93b45c13ee7f1ba942ebfe698e335126b7529bad
Pedro Beduschi <pe.beduschi@gmail.com>
2022-09-08 18:06:22 -0400

    Colocando a form na página certa

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,1 +1,20 @@
 console.log('Código javascript')
+
+let linha = 
+[
+    {
+        name: 'Pedro Beduschi',
+        tel: '19123456789',
+        xp: true
+    },
+    {
+        name: 'João da Silva',
+        tel: '19123456789',
+        xp: false
+    },
+    {
+        name: 'Donivete Irene',
+        tel: '19123456789',
+        xp: true
+    },
+]
